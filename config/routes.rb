ca8992bebf78fd811a5b7a0c97394f41452d0e82
EitanFuturo <llovia@gmail.com>
2021-03-10 02:03:26 -0300

    Se modifica el estilo de la vista show.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'messages#index'
   resources :messages
   post 'messages/decrypt_message', to: 'messages#decrypt_message', as: :decrypt_message
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
