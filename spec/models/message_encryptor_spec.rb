4f6530094f1b812e28b20146a39ec8e80ffdffaf
EitanFuturo <llovia@gmail.com>
2021-08-07 00:32:39 -0300

    Se agrega un test.

--- spec/models/message_encryptor_spec.rb
+++ spec/models/message_encryptor_spec.rb
@@ -4,11 +4,19 @@ describe MessageEncryptor do
 
   text = 'Bill'
   password = 'Evans'
+  error_message = "Contraseña inválida"
 
   let(:message) { Message.create(text: text, password: password, confirmed_password: password) }
 
+
   it 'returns same input message when decrypted' do
-    decrypted_message = subject.decrypt(message.text, password, message.iv) rescue "Contraseña inválida"
+    decrypted_message = subject.decrypt(message.text, password, message.iv) rescue error_message
     expect(decrypted_message).to eq text
+  end
+
+  it 'returns invalid password message when password is not correct' do
+    decrypted_message = subject.decrypt(message.text, password.reverse, message.iv) rescue error_message
+    expect(decrypted_message).to eq error_message
+    true
   end
 end
